383d369ccba3745088d21954a13455b929bb7d34
harcolemas@gmail.com <harcolemas@gmail.com>
2023-12-03 09:55:49 +0500

    Made the base of the entire site

---
+++ script.js
@@ -0,0 +1,15 @@
+  function hideSlogan() {
+    document.querySelector('.slogan').classList.add('hidden');;
+    document.querySelector('.rightIcons').classList.add('hidden');;
+    document.querySelector('.searchContainer').style.width = '1162px';;
+    document.querySelector('.searchNav').style.width = '1130px';;
+  document.querySelector('.searchContainer').querySelector('.material-symbols-outlined').textContent = 'close';
+  }
+
+  function showSlogan() {
+    document.querySelector('.searchNav').style.width = '340px';;
+    document.querySelector('.searchContainer').style.width = '372px';;
+    document.querySelector('.slogan').classList.remove('hidden');;
+    document.querySelector('.rightIcons').classList.remove('hidden');;
+  document.querySelector('.searchContainer').querySelector('.material-symbols-outlined').textContent = 'search';
+  }
